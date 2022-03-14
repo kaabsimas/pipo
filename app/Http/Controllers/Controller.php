d00ef61a9d27d6dc32345a1aeffea4b3686020c8
Albert Melo <albert_usm@hotmail.com>
2022-03-14 17:03:53 -0300

    Primeiro commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function createEmployee()
+    {
+        $users = User::get();
+        \Auth::login($users->random());
+
+        return view('main', compact('users'));
+    }
+}
